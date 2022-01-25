189b558adb383c07fbb1f36b459fc0a2d7fede80
owaisahmedkhan <owaisahmedkhan@.github.com>
2022-01-25 19:08:17 +0500

    heroku worksstart here for auto deploy 2

--- index.mjs
+++ index.mjs
@@ -3,15 +3,15 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-  res.send("Hello World!, Your Node .mjs Server is running and listening at Port:"+ port)
+  res.send("Hello World!, Your Node .mjs Server is running and listening at Port:"+ port + "<br/> Now this is automatic deployed with heroku. make any changes at github and its done automatically. Hurray !!")
 })
 
 app.get('/water', (req, res) => {
-    res.send("Hello World!, Take the water")
+    res.send("Hello World!, Take the water 1!")
   })
 
 app.get('/food', (req, res) => {
-    res.send("Hello World!, here is your food")
+    res.send("Hello World!, here is your food !!")
   })
 
 app.listen(port, () => {
